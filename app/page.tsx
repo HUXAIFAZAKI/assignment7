a82a084f1f0692026d618b268bd8831cf13f8472
huzaifa <huzaifazaki92@gmail.com>
2025-01-01 23:00:55 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,56 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const fetchData = async () => {
+    try {
+      const data = await fetch('https://fakestoreapi.com/products')
+      const res = await data.json();
+      return res;
+    }
+    catch (error) {
+      return error;
+    }
+  }
+  const data = await fetchData();
+  
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <>
+    <nav className="bg-black text-white">
+      <ul className="flex gap-4 justify-center p-4 text-2xl lg:text-4xl">
+        <li className="select-none cursor-pointer">Home</li>
+        <li className="select-none cursor-pointer">Cart</li>
+        <li className="select-none cursor-pointer">Profile</li>
+        <li className="select-none cursor-pointer">Logout</li>
+      </ul>
+    </nav>
+    <main className="w-full mx-auto p-8 bg-[#f0f0f0]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-2">
+      {
+            data.map((item: any) => (
+              <div key={item.id} className="bg-white flex flex-col items-center gap-2 justify-between border border-black w-[full] rounded-lg p-4 hover:shadow-2xl cursor-pointer duration-100">
+                <h1 className="font-bold text-2xl">{item.title}</h1>
+                <Image src={item.image} alt={item.title} width={124} height={100} className="select-none"/>
+                <p className="">{item.description}</p>
+                <span className="flex gap-2 justify-between w-full">
+                  <span className="flex gap-4 justify-between">
+                    <p className="text-gray-500 font-bold text-2xl line-through">${item.price}</p>
+                    <p className="font-bold text-2xl">${Math.floor(Math.abs(item.price-10))}</p>
+                  </span>
+                  <p className="font-bold text-2xl">⭐{item.rating.rate}</p>
+                </span> 
+              </div>
+            ))
+        }
+      </div>
+    </main>
+    <footer className="bg-black text-white ">
+      <div className="flex flex-col items-center gap-4 justify-center p-4 text-4xl">
+        <h2 className="font-bold text-2xl">Footer</h2>
+        <p className="font-bold text-xl">Assignment 7 using fakestoreapi</p>
+        <p className="font-bold text-xl">Copyright &copy; 2023</p>
+      </div>
+      
+    </footer>
+    </>
   );
 }
